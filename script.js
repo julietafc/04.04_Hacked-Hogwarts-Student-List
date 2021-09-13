4f6a971f7e09de9bd08401d35b822e2e3fa94e84
julietafc <juli47g1@stud.kea.dk>
2021-09-13 10:16:34 +0200

    fetch json students

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ function start() {
   selectedButton();
   loadJson();
 }
+
+function selectedButton() {
+  document.querySelectorAll("[data-action='filter']").forEach((button) => button.addEventListener("click", selectFilter));
+}
+
+function loadJson() {
+  fetch("https://petlatkea.dk/2021/hogwarts/students.json")
+    .then((res) => res.json())
+    .then((data) => {
+      prepareObjects(data);
+    });
+}
+
+function prepareObjects(jsonData) {
+  jsonData.forEach((object) => {
+    // console.log("student", object);
+    const student = Object.create(Student);
+    student.house = getHouse(object.house);
+    student.firstname = getFirstName(object.fullname);
+    student.middlename = getMiddleName(object.fullname);
+    student.nickname = getNickName(object.fullname);
+    student.lastname = getLastName(object.fullname);
+    student.image = getImage(object.lastname, object.firstname);
+
+    allStudents.push(student);
+  });
+
+  displayList(allStudents);
+}
